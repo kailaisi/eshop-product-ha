8f708251ea202b31abceb40b738e63d7aa357751
wujinxiang <wujinxiang@tcsl.com.cn>
2019-04-25 17:33:34 +0800

    增加批量查询接口

--- src/main/java/com/kailaisi/controller/ProductController.java
+++ src/main/java/com/kailaisi/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.kailaisi.controller;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,5 +20,20 @@ public class ProductController {
                 "\"price\": 5599, \"pictureList\":\"a.jpg,b.jpg\", \"specification\": \"iphone7的规格\"," +
                 " \"service\": \"iphone7的售后服务\", \"color\": \"红色,白色,黑色\", \"size\": \"5.5\"," +
                 " \"shopId\": 1, \"modifiedTime\": \"2017-01-01 12:01:00\",\"cityId\":1,\"brandId\":1}";
+    }
+
+    @RequestMapping("/getProductInfos")
+    @ResponseBody
+    public String getProductInfos(String productIds) {
+        String[] ids = productIds.split(",");
+        JSONArray jsonArray = new JSONArray();
+        for (String id : ids) {
+            String info = "{\"id\":" + id + ", \"name\": \"iphone7手机\", " +
+                    "\"price\": 5599, \"pictureList\":\"a.jpg,b.jpg\", \"specification\": \"iphone7的规格\"," +
+                    " \"service\": \"iphone7的售后服务\", \"color\": \"红色,白色,黑色\", \"size\": \"5.5\"," +
+                    " \"shopId\": 1, \"modifiedTime\": \"2017-01-01 12:01:00\",\"cityId\":1,\"brandId\":1}";
+            jsonArray.add(JSONObject.parseObject(info));
+        }
+        return jsonArray.toJSONString();
     }
 }
